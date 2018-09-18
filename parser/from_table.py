b8626748567ae39dfc2582cb0af47c801a3baa23
iitProgect <42654208+iitProgect@users.noreply.github.com>
2018-09-18 10:40:45 +0300

    try yargy

--- parser/from_table.py
+++ parser/from_table.py
@@ -7,16 +7,15 @@ import pymorphy2
 morph = pymorphy2.MorphAnalyzer()
 
 
+
 def ExistWordFromDictInParagraph(list_words, paragraph):
     words = re.findall(r"[-\w']+", paragraph)
     words = [morph.parse(i)[0].normal_form for i in words]
     return any(elem in list_words for elem in words)
 
-document = Document('C://Users//Катя//Desktop//11_РПД _Программирование (2).docx')
-# document = Document('C://Users//Катя//Desktop//ПИГА 09.03.01, 2016, (4.0), Информатика и вычислительная техника (19610).docx')
+#document = Document('C://Users//Катя//Desktop//11_РПД _Программирование (2).docx')
+document = Document('C://Users//Катя//Desktop//ПИГА 09.03.01, 2016, (4.0), Информатика и вычислительная техника (19610).docx')
 
-
-text = document.paragraphs
 
 def FindTable():
     for table in document.tables:
