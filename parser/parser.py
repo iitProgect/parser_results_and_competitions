7c3c6cdf7a9b50c2a3e7d4633ad3e074b557a9ad
iitProgect <42654208+iitProgect@users.noreply.github.com>
2018-09-29 13:35:55 +0500

    test document

--- parser/parser.py
+++ parser/parser.py
@@ -35,9 +35,7 @@ IsResults = rule(dictionary(
 ))
 TableIsCompetitionsAndResults = rule(dictionary(
     {
-        'результат',
-        'ФГОС',
-        'компетенция'
+        'результат'
     }
 ))
 
@@ -70,8 +68,13 @@ def FindFGOS(text):
     return code_FGOS
 
 
-document = Document('C://Users//Катя//Desktop//РПД Государственная итоговая аттестация.docx')
+# document = Document('C://Users//Катя//Desktop//рпд//РПД Государственная итоговая аттестация.docx')
+# document = Document('C://Users//Катя//Desktop//рпд//РПД Аналитика информационных систем (09.03.01, 2016, (4.0), Информатика и вычислительная техника(19610)).docx')
+# document = Document('C://Users//Катя//Desktop//рпд//РПД _Дискретная математика по БИ_38.03.05.docx')
 
+# document = Document('C://Users//Катя//Desktop//рпд//4_РПД Экономика.docx')
+# document = Document('C://Users//Катя//Desktop//рпд//1.История.docx')
+document = Document('C://Users//Катя//Desktop//рпд//2.Философия.docx')
 
 def FindTable():
     for table in document.tables:
@@ -79,42 +82,69 @@ def FindTable():
         data = []
 
         keys = None
-        for i, row in enumerate(table.rows):
-            text = (cell.text for cell in row.cells)
-
-            # Establish the mapping based on the first row
-            # headers; these will become the keys of our dictionary
-            if i == 0:
-                keys = tuple(text)
-                continue
-
-            # Construct a dictionary for this row, mapping
-            # keys to values for this row
-            row_data = dict(zip(keys, text))
-            data.append(row_data)
 
 
-        for j in data[0]:
-            if parser_Table.find(j) is not None:
-                return data
+        if table.rows[0].cells[0].text == table.rows[0].cells[1].text:
+            index_row = 0
+
+
+            for i in range(len(table.rows)-1):
+                if index_row == 0:
+                    index_row += 1
+                    continue
+                row_data = {}
+                index_column = 0
+
+                for j in table.columns:
+                    if index_column == 2 and table.rows[1].cells[index_column].text == '':
+                        row_data[table.rows[0].cells[index_column].text] = table.rows[index_row + 1].cells[index_column].text
+                    row_data[table.rows[1].cells[index_column].text] = table.rows[index_row+1].cells[index_column].text
+                    index_column += 1
+                data.append(row_data)
+                index_row += 1
+            print(data)
+
+        else:
+            for i, row in enumerate(table.rows):
+
+                text = (cell.text for cell in row.cells)
+                if i == 0:
+                    keys = tuple(text)
+                    continue
+                row_data = dict(zip(keys, text))
+                data.append(row_data)
+
+        if len(data) != 0:
+            for j in data[0]:
+                if parser_Table.find(j) is not None:
+                    print(data)
+                    return data
+        else:
+            continue
+
+    return None
 
 FGOS = []
 competitions = []
 results = []
 
 table = FindTable()
+
 if table is not None:
     #определение какой столбец за что отвечает
     key = 0
     dict={}
     for i in table[0]:
-        if parser_FGOS.find(i) is not None:
+        if parser_FGOS.find(i) is not None and 'ФГОС' not in dict:
+            print('ФГОС '+i)
             dict['ФГОС'] = i
-        elif parser_Competition.find(i) is not None:
+        elif parser_Competition.find(i) is not None and 'Компетенция' not in dict:
+            print('Компетенция ' + i)
             dict['компетенции'] = i
-        elif parser_Result.find(i) is not None:
+        elif parser_Result.find(i) is not None and 'Результаты' not in dict:
+            print('Результаты ' + i)
             dict['результаты'] = i
-
+    print(dict)
     for i in table:#i - словарь
         if 'компетенции' in dict and not i[dict['компетенции']] == dict['компетенции']:
             competitions.append(i[dict['компетенции']])
@@ -123,6 +153,10 @@ if table is not None:
         if 'ФГОС' in dict:
             FGOS.append(i[dict['ФГОС']])
 
+    print(FGOS)
+    print(competitions)
+    print(results)
+    print('Сущности')
     if len(results) != 0:
         if len(competitions)!= 0 and len(FGOS) != 0:
             competition_result = []
