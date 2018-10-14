0a9f22d4155456c592f75c5c37a44c47378cd149
iitProgect <42654208+iitProgect@users.noreply.github.com>
2018-10-15 00:20:47 +0500

    try

--- parser/main_information.py
+++ parser/main_information.py
@@ -1,6 +1,6 @@
 from yargy import Parser, rule, and_, or_
 from yargy.predicates import gram, is_capitalized, dictionary, is_upper, length_eq, is_title
-from docx import Document, section
+from docx import Document
 from docx.shared import Inches
 import os, subprocess
 
@@ -14,6 +14,32 @@ def GetDocuments():
     return documents
 
 documents = GetDocuments()
-path = baseDir + '\\'+documents[6]
+path = baseDir + '\\'+documents[0]
 print(path)
 document = Document(path)
+
+from yargy import Parser, rule, and_
+from yargy.predicates import gram, is_capitalized, dictionary
+
+
+Name = rule(
+    and_(
+        gram('ADJF'),
+        or_(
+        is_capitalized(),
+        is_upper()
+        )                 # http://pymorphy2.readthedocs.io/en/latest/user/grammemes.html
+    ),
+    gram('ADJF').optional().repeatable(),
+    dictionary({
+        'институт',
+        'кафедра',
+        'университет'
+    })
+)
+print(document.)
+
+parser = Parser(Name)
+for i in document.paragraphs:
+    for match in parser.findall(i.text):
+        print([_.value for _ in match.tokens])
